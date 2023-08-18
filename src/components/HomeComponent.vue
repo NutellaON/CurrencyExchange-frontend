86f4dcb8576d2541f5ddb7a3f7628e012aa27058
alanscertkojevs@scandiweb.com <alans.certkojevs@gmail.com>
2023-08-18 13:04:16 +0300

    main page

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>Daily currency exchanger rate!</h1>
+  <div class="container">
+    <div class="currency-box" @click="goToExchangeRate('USD')">
+      EUR to USD Exchange Rate
+    </div>
+    <div class="currency-box" @click="goToExchangeRate('GBP')">
+      EUR to GBP Exchange Rate
+    </div>
+    <div class="currency-box" @click="goToExchangeRate('AUD')">
+      EUR to AUD Exchange Rate
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeComponent',
+  methods: {
+    goToExchangeRate(currency) {
+      this.$router.push(`/exchange-rates/${currency}`);
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1{
+  text-align: center;
+  padding-bottom: 50px;
+}
+.container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.currency-box {
+  padding: 10px 40px;
+  border: 1px solid #ccc;
+  margin: 10px;
+  cursor: pointer;
+}
+</style>
